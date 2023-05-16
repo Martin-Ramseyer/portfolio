9f02b367224526179b5e470d2e27c18703c0a13a
Martin-Ramseyer <martinramseyer4@gmail.com>
2023-05-16 14:04:22 -0300

    cambios form.php

--- form.php
+++ form.php
@@ -1,26 +1,23 @@
 <?php
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-  $nombre = $_POST["nombre"];
-  $correo = $_POST["correo"];
-  $asunto = $_POST["asunto"];
-  $mensaje = $_POST["mensaje"];
+    $nombre = $_POST["nombre"];
+    $correo = $_POST["correo"];
+    $asunto = $_POST["asunto"];
+    $mensaje = $_POST["mensaje"];
 
-  // Configuración de los datos del correo
-  $destinatario = "martinramseyer4@gmail.com";
-  $asuntoCorreo = "Nuevo mensaje de contacto";
-  $mensajeCorreo = "Nombre: " . $nombre . "\n";
-  $mensajeCorreo .= "Correo electrónico: " . $correo . "\n";
-  $mensajeCorreo .= "Asunto: " . $asunto . "\n";
-  $mensajeCorreo .= "Mensaje: " . $mensaje . "\n";
+    // Configuración de los datos del correo
+    $destinatario = "martinramseyer4@gmail.com";
+    $asuntoCorreo = "Nuevo mensaje de contacto";
+    $mensajeCorreo = "Nombre: " . $nombre . "\n";
+    $mensajeCorreo .= "Correo electrónico: " . $correo . "\n";
+    $mensajeCorreo .= "Asunto: " . $asunto . "\n";
+    $mensajeCorreo .= "Mensaje: " . $mensaje . "\n";
 
-  mail($destinatario, $asuntoCorreo, utf8_decode($mensajeCorreo));
-
-  // Envío del correo
-  if (mail($destinatario, $asuntoCorreo, $mensajeCorreo)) {
-    echo "El correo ha sido enviado correctamente.";
-  } else {
-    echo "Hubo un error al enviar el correo.";
-  }
+    // Envío del correo
+    if (mail($destinatario, $asuntoCorreo, $mensajeCorreo)) {
+        echo "El correo ha sido enviado correctamente.";
+    } else {
+        echo "Hubo un error al enviar el correo.";
+    }
 }
 ?>
-
