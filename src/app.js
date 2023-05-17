42f187138e146a43015a605e77e5feddfa315bcb
Martin-Ramseyer <martinramseyer4@gmail.com>
2023-05-16 23:01:41 -0300

    agregando dark-mode

--- src/app.js
+++ src/app.js
@@ -1,10 +1,37 @@
-var menuItems = document.querySelectorAll('.navbar-nav .nav-link');
-menuItems.forEach(function(item) {
-  item.addEventListener('click', function() {
-    // Cierra el menú automáticamente al hacer clic en un enlace
+document.addEventListener("DOMContentLoaded", function () {
+  const darkModeToggle = document.getElementById("dark-mode-toggle");
+  const darkModeImage = darkModeToggle.querySelector("img");
+  const logo_contacto = document.getElementById("logo_contacto");
+
+  darkModeToggle.addEventListener("click", function () {
+    document.body.classList.toggle("dark-mode");
+    const isDarkMode = document.body.classList.contains("dark-mode");
+
+    if (isDarkMode) {
+      darkModeImage.src = "src/images/modo-on.png";
+      logo_contacto.src = "src/images/logo-dark.png";
+    } else {
+      darkModeImage.src = "src/images/modo-off.png";
+      logo_contacto.src = "src/images/logo.png";
+    }
+
+
     var menuToggle = document.querySelector('.navbar-toggler');
     var navbarCollapse = document.querySelector('.navbar-collapse');
-    menuToggle.classList.add('collapsed');
-    navbarCollapse.classList.remove('show');
+    if (navbarCollapse.classList.contains('show')) {
+      menuToggle.classList.add('collapsed');
+      navbarCollapse.classList.remove('show');
+    }
+  });
+
+  var menuItems = document.querySelectorAll('.navbar-nav .nav-link');
+  menuItems.forEach(function (item) {
+    item.addEventListener('click', function () {
+
+      var menuToggle = document.querySelector('.navbar-toggler');
+      var navbarCollapse = document.querySelector('.navbar-collapse');
+      menuToggle.classList.add('collapsed');
+      navbarCollapse.classList.remove('show');
+    });
   });
 });
